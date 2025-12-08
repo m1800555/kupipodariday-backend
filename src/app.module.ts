1ccbe7b65da7e157b75cabe5a38ba72c2110abc5
Anton Kuzmenko <m1800555@yandex.ru>
2025-12-08 15:44:23 +0300

    Add offer, user, wish, wishlist entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { OffersModule } from './offers/offers.module';
+import { UsersModule } from './users/users.module';
+import { WishesModule } from './wishes/wishes.module';
+import { WishlistsModule } from './wishlists/wishlists.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'student',
+      password: 'student',
+      database: 'kupipodariday',
+      entities: [],
+      synchronize: true,
+    }),
+    OffersModule,
+    UsersModule,
+    WishesModule,
+    WishlistsModule,
+  ],
+  controllers: [],
   providers: [],
 })
 export class AppModule {}
